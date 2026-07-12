212706eb9a4cecd1f79bf0ea18324c845747c45c
BHALLAMUDI KAMESWARI SATHVIKA <bsathvika8804@gmail.com>
2026-07-12 20:21:56 +0530

    Add Exercise 3 - Assertions in JUnit

---
+++ TDD-using-JUnit5-and-Mockito/Exercise-3-Assertions in JUnit/Code/AssertionsTest.java
@@ -0,0 +1,21 @@
+package spring_learn;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AssertionsTest {
+
+    @Test
+    public void testAssertions() {
+        // Assert equals
+        assertEquals(5, 2 + 3);
+        // Assert true
+        assertTrue(5 > 3);
+        // Assert false
+        assertFalse(5 < 3);
+        // Assert null
+        assertNull(null);
+        // Assert not null
+        assertNotNull(new Object());
+    }
+}
