3a82a4bf4d03883d6c1e387aa9c73ab6b4bb1db3
BHALLAMUDI KAMESWARI SATHVIKA <bsathvika8804@gmail.com>
2026-07-19 13:51:19 +0530

    Add Hands-on 2 - React Components (StudentApp)

---
+++ week-07-React/hands-on-2-ReactJS-HOL/code/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the About page of the Student Management Portal</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
